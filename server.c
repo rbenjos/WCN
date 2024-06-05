1d49119bc4e5b4c81e6ea3904c97494f8d96c438
rbenjos <rbenjos@gmail.com>
2024-06-05 11:08:15 +0300

    Update server.c

--- server.c
+++ server.c
@@ -1,71 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
-#define PORT 8888  // The port number on which the server will listen
-
-int main()
+int main(void)
 {
-  printf("%s\n", "starting");
-  fflush(stdout);
-  int server_fd, new_socket, valread;
-  struct sockaddr_in address;
-  int addrlen = sizeof(address);
-  char buffer[1 << 20] = {0};  // Buffer to store incoming data from the client
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-  // Creating socket file descriptor
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-      perror("socket failed\n");
-      exit(EXIT_FAILURE);
-    }
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(8080); // Port 8080
+    address.sin_addr.s_addr = INADDR_ANY;
 
-  address.sin_family = AF_INET;  // Address family: IPv4
-  address.sin_addr.s_addr = INADDR_ANY;  // Bind to any available network interface
-  address.sin_port = htons(PORT);  // Convert the port number to network byte order
+    // Bind server_socket to address
+    bind(server_socket, (struct sockaddr *) &address, sizeof(address));
 
-  // Forcefully attaching socket to the port 8888
-  if (bind(server_fd, ( struct sockaddr * ) &address, sizeof(address)) < 0)
-    {
-      perror("bind failed\n");
-      exit(EXIT_FAILURE);
-    }
+    // Listen for clients and allow the accept function to be used
+    // Allow 4 clients to be queued while the server processes
+    listen(server_socket, 4);
 
-  // Listening for incoming connections
-  if (listen(server_fd, 3) < 0)
-    {
-      perror("listen\n");
-      exit(EXIT_FAILURE);
-    }
+    // Wait for client to connect, then open a socket
+    int client_socket = accept(server_socket, NULL, NULL);
 
-  // Accepting incoming connection
-  if ((new_socket = accept(server_fd, ( struct sockaddr * ) &address, ( socklen_t * ) &addrlen))< 0)
-    {
-      perror("accept\n");
-      exit(EXIT_FAILURE);
-    }
+    char message[] = "Hello, World!";
 
-  while (strcmp(buffer, "EXPERIMENT COMPLETE") != 0)
-    {
-      valread = read(new_socket, buffer,1 << 20); // Reading data from the client
-      printf("Received: %s\n", buffer);
-      fflush(stdout);
-      send(new_socket, "PACKET ARRIVED", strlen("PACKET ARRIVED"), 0);
-      if (strcmp(buffer, "PHASE COMPLETE") == 0)
-        {
-          send(new_socket, "RECEIVED", strlen("RECEIVED"), 0);
-        }
-    }
+    // Send message to the client
+    send(client_socket, message, strlen(message), 0);
 
-  // Sending a response back to the client
-  send(new_socket, "EXPERIMENT COMPLETE", strlen("EXPERIMENT COMPLETE"), 0);
-  printf("EXPERIMENT COMPLETE, SHUTTING DOWN\n");
+    // Close the client socket
+    close(client_socket);
 
-  // Closing the connected socket
-  close(new_socket);
-  shutdown(server_fd, SHUT_RDWR);
-  return 0;
+    return 0;
 }
