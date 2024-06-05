76b7b9d7e9a42ee628eeced8a8857597f250ceab
rbenjos <rbenjos@gmail.com>
2024-06-05 12:35:25 +0300

    Update client.c

--- client.c
+++ client.c
@@ -48,7 +48,7 @@ int main(void)
     memset(message, 0, 256);
     send(client_socket, "STARTING", strlen("STARTING"), 0);
 
-    recv(client_socket, message, 255, 0) > 0);
+    recv(client_socket, message, 255, 0);
     printf("%s\n", message);
     fflush(stdout);
     send_mul_packets(client_socket, 16,20);
