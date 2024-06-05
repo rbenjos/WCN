27372b55871894f8b5af8aa88e92c92e2e3bddb7
rbenjos <rbenjos@gmail.com>
2024-06-05 12:07:27 +0300

    Update client.c

--- client.c
+++ client.c
@@ -10,7 +10,7 @@
 
 void send_packet(int sock, int packet_size){
   char *packet;
-  packet = (char *) calloc(packet_size);
+  packet = (char *) malloc(packet_size);
   send(sock, packet, packet_size, 0);
 }
 
